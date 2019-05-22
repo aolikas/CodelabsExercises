00b0edf8db67d083e7cf511c275961a659cca14f
aolikas <olkaspark@gmail.com>
2019-05-22 18:48:30 +0200

    support landscape, tablets

--- CodelabsMaterialMe/app/src/main/java/com/example/codelabsmaterialme/MainActivity.java
+++ CodelabsMaterialMe/app/src/main/java/com/example/codelabsmaterialme/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.res.TypedArray;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
@@ -25,20 +26,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //initialize the RecyclerView
         mRecyclerView = findViewById(R.id.recycler_view);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        //Get the appropriate  column count
+        int gridColumnCount = getResources().getInteger(R.integer.grid_column_count);
+
+        //set layout manager
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, gridColumnCount));
+
+        //initialize the ArrayList that will contain the data
         mSportsData = new ArrayList<>();
 
+        // initialize the adapter and set it to the RecyclerView
         mAdapter = new SportsAdapter(this, mSportsData);
         mRecyclerView.setAdapter(mAdapter);
 
+        //get the data
         initializeData();
 
+        //if there is more than one column, display swipe to dismiss
+        int swipeDirs;
+        if(gridColumnCount > 1) {
+            swipeDirs = 0;
+        } else {
+            swipeDirs = ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT;
+        }
+
+        //helper class for creating swipe to dismiss and drag and drop functionality
         ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper.
                 SimpleCallback(ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT |
-                ItemTouchHelper.DOWN | ItemTouchHelper.UP, ItemTouchHelper.LEFT |
-                ItemTouchHelper.RIGHT) {
+                ItemTouchHelper.DOWN | ItemTouchHelper.UP, swipeDirs) {
+
             @Override
             public boolean onMove(@NonNull RecyclerView recyclerView,
                                   @NonNull RecyclerView.ViewHolder viewHolder,
