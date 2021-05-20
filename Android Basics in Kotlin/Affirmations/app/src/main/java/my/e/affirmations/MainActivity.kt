1531f2255ed96210dd04d2915ffc0e1ac3649c01
aolikas <46649354+aolikas@users.noreply.github.com>
2021-05-20 17:36:31 +0200

    Display a scrollable list

---
+++ Android Basics in Kotlin/Affirmations/app/src/main/java/my/e/affirmations/MainActivity.kt
@@ -0,0 +1,18 @@
+package my.e.affirmations
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import my.e.affirmations.adapter.ItemAdapter
+import my.e.affirmations.data.Datasource
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val myDataset = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
+    }
+}
---
+++ Android Basics in Kotlin/Affirmations/app/src/main/java/my/e/affirmations/model/Affirmation.kt
@@ -0,0 +1,9 @@
+package my.e.affirmations.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
